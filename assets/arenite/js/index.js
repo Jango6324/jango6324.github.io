c99d6d5b39c309dd2b1066d42ac4a79460badaab
Jango6324 <zhanyuy010@gmail.com>
2026-04-29 16:38:39 -0400

    Add Woodstock Project Page

--- assets/arenite/js/index.js
+++ assets/arenite/js/index.js
@@ -15,6 +15,43 @@ $(document).ready(function() {
 
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
+
+		function restartAutoplay(carousel) {
+			if (!carousel || !carousel.options || !carousel.options.autoplay) {
+				return;
+			}
+
+			if (typeof carousel.stop === 'function') {
+				carousel.stop();
+			}
+			if (typeof carousel.start === 'function') {
+				carousel.start();
+			}
+		}
+
+		function isManualControlTarget(target) {
+			if (!target || typeof target.closest !== 'function') {
+				return false;
+			}
+
+			return !!target.closest('.slider-navigation-next, .slider-navigation-previous, .slider-page');
+		}
+
+		(carousels || []).forEach(function(carousel) {
+			if (!carousel || !carousel.wrapper) {
+				return;
+			}
+
+			carousel.wrapper.addEventListener('click', function(event) {
+				if (isManualControlTarget(event.target)) {
+					restartAutoplay(carousel);
+				}
+			});
+
+			carousel.wrapper.addEventListener('touchend', function() {
+				restartAutoplay(carousel);
+			}, { passive: true });
+		});
 	
     bulmaSlider.attach();
 
